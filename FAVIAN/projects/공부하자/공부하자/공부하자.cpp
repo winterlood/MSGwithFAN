5e058a8c0af319c5351d8acc0d97e728e9d9eb1f
Favian <damin8@naver.com>
2019-07-24 02:14:52 +0900

    ss

---
+++ FAVIAN/projects/공부하자/공부하자/공부하자.cpp
@@ -0,0 +1,24 @@
+﻿#include<algorithm>
+#include<iostream>
+using namespace std;
+
+int answer[10] = { 1,2,3,4,5,1,2,3,4,5 };
+bool flag;
+int main() {
+	int n, x;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < 10; j++) {
+			cin >> x;
+			if (x != answer[j])
+				flag = true;
+		}
+		if (flag) {
+			flag = false;
+			continue;
+		}
+		else {
+			cout << i << "\n";
+		}
+	}
+}
